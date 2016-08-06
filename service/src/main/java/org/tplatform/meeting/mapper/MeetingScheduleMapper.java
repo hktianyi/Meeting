691bdc1ba70cbe7ef362a59581a98d1312faa4f1
wuzhiliang <252886919@qq.com>
2016-08-06 17:25:28 +0800

    会议服务类

---
+++ service/src/main/java/org/tplatform/meeting/mapper/MeetingScheduleMapper.java
@@ -0,0 +1,13 @@
+package org.tplatform.meeting.mapper;
+
+import org.tplatform.meeting.entity.MeetingSchedule;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ *
+ * Created by wzl on 2016/8/6.
+ */
+public interface MeetingScheduleMapper extends Mapper<MeetingSchedule> {
+
+
+}
---
+++ service/src/main/java/org/tplatform/meeting/service/MeetingInfoService.java
@@ -0,0 +1,12 @@
+package org.tplatform.meeting.service;
+
+import org.tplatform.impl.BaseService;
+import org.tplatform.meeting.entity.MeetingInfo;
+
+/**
+ * Created by wzl on 2016/8/6.
+ */
+public class MeetingInfoService extends BaseService<MeetingInfo> {
+
+
+}
