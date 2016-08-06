20034c5746010d117115a70a91475ae016601139
wuzhiliang <252886919@qq.com>
2016-08-06 15:13:32 +0800

    会议基本信息

---
+++ service/src/main/java/org/tplatform/meeting/entity/MeetingInfo.java
@@ -0,0 +1,17 @@
+package org.tplatform.meeting.entity;
+
+import lombok.Data;
+import org.tplatform.core.entity.BaseEntity;
+
+import javax.persistence.Table;
+
+/**
+ * Created by wzl on 2016/8/6.
+ */
+@Data
+@Table(name = "t_meetingInfo")
+public class MeetingInfo extends BaseEntity {
+
+
+
+}
