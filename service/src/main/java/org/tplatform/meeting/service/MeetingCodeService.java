384c28b382c12907552571f5a5d2bbc32cbc78b5
zjkforever@163.com <123jian456>
2016-08-17 11:07:19 +0800

    会议会议

--- service/src/main/java/org/tplatform/meeting/service/MeetingCodeService.java
+++ service/src/main/java/org/tplatform/meeting/service/MeetingCodeService.java
@@ -1,12 +1,17 @@
 package org.tplatform.meeting.service;
 
+import com.foxinmy.weixin4j.mp.model.QRParameter;
+import com.foxinmy.weixin4j.mp.model.QRResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tplatform.impl.BaseService;
 import org.tplatform.meeting.entity.MeetingCode;
 import org.tplatform.meeting.mapper.MeetingCodeMapper;
+import org.tplatform.util.WXUtil;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zjk on 2016/8/13.
@@ -24,6 +29,20 @@ public class MeetingCodeService extends BaseService<MeetingCode> {
         }else{
             return null;
         }
+
+
+    }
+
+    public Map<String, Object> geneQrcode(){
+        //WXUtil.getProxy("1").sendTmplMessage(templateMessage);
+
+//        try {
+//            QRParameter parameter = new QRParameter();
+//            QRResult res = WXUtil.getProxy("1").createQR(parameter);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+        return null;
     }
 
 }
